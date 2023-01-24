15ce8464337e0744579bf603e941be769bebe2c6
Eduardo Vieira <122796060+Eduhsvieira@users.noreply.github.com>
2023-01-24 19:39:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const calcular = document.getElementById ('calcular');
+
+
+
+function imc() {
+    const nome = document.getElementById ('nome').value;
+    const altura = document.getElementById ('altura').value;
+    const peso = document.getElementById ('peso').value;
+    const resultado = document.getElementById ('resultado');
+
+    if (nome !== '' && peso !== '' && altura !== '' ) {
+        
+        const valorIMC = (peso/ (altura * altura)).toFixed(1);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.';
+        }
+
+        else if (valorIMC < 25){
+            classificacao = 'Com o peso ideal.';
+        }
+
+        else if (valorIMC < 30){
+            classificacao = 'Levemente acima do peso.';
+        }
+
+        else if (valorIMC < 35){
+            classificacao = 'Obesidade grau I.';
+        }
+
+        else if (valorIMC < 40){
+            classificacao = 'Obesidade grau II.';
+        }
+
+        
+        else {
+            classificacao = 'Obesidade grau III. Cuidado!';
+        }
+
+
+        resultado.textContent = `${nome}, Seu IMC é ${valorIMC}, e você está ${classificacao}. `;
+
+    }
+    else {
+        resultado.textContent = 'Preencha todos os campos!';
+    }
+    
+    
+
+}
+
+
+calcular.addEventListener('click', imc);
